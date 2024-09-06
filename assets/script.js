8abfab183f95e1524c04babe27028d4f39acfddc
Keaton007 <keatonjonesy@gmail.com>
2024-09-05 21:38:02 -0600

    Some minor changes to the HTML, CSS, and added some JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
-// TODO: ALL OF IT
+document.addEventListener('DOMContentLoaded', function () {
+    const box = document.querySelectorAll('.interactions box');
+    const text = document.querySelectorAll('imgText');
+
+    box.forEach((box , index) => {
+        box.addEventListener('click', function () {
+            text.forEach(text => text.style.display = 'none');
+            text[index].style.display = 'block';
+        });
+    });
+});
