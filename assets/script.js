af7d16b84876bc70ef4a16e21012b9c749c34658
nicktischmak <nicktischmak@gmail.com>
2024-09-09 15:45:25 -0600

    some layout changes and text shows while hovering

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const text = document.querySelectorAll('imgText');
 
     box.forEach((box , index) => {
-        box.addEventListener('click', function () {
+        box.addEventListener('hover', function () {
             text.forEach(text => text.style.display = 'none');
             text[index].style.display = 'block';
         });
